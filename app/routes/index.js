0ab87328238e10c52f3ccde3cce57024e2c30bb8
davidwillianx <davidcbsi@gmail.com>
2015-07-01 01:27:32 -0300

    paths organized

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/',function(req,res){
-    res.send('de olho');
+  res.render('index.ejs',{tomato: 'David'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 var http = require('http');
 var express = require('express');
+var logger = require('morgan');
+var path  = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-var app = express();
+//waiting for passport and socket.io
+
+var flash = require('connect-flash');
+
 var user = require('./app/routes/user');
 var ini = require('./app/routes/index');
 
+var app = express();
+
+app.set('views',path.join(__dirname,'app/views'));
+app.set('views egine', 'ejs ');
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'youshouldbebetterasfasterasyoucan',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
 
 app.use('/',ini);
 app.use('/user',user);
 
+app.use(express.static(__dirname+'/app/public'));
 
 var server = http.createServer(app);
-server.listen(8080,function(){
-    console.log('here we comes');
+
+server.listen(app.get('port'),function(){
+  console.log('Server running');
 });
