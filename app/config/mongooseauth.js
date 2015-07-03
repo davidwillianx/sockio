04edaac344696ff8e9ff525a883458fd5a3ea4ab
davidwillianx <davidcbsi@gmail.com>
2015-07-03 02:49:39 -0300

    database confg

--- app/config/mongooseauth.js
+++ app/config/mongooseauth.js
@@ -1,2 +1,5 @@
 
-module.exports = { 'url':'mongodb://localhost/sockio'};
+module.exports = {
+  //  'url':'mongodb://localhost/sockio'
+  'url':'mongodb://davidwillianx:pitbul90@ds061318.mongolab.com:61318/sockio'
+};
