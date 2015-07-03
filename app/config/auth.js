3751ca89367bfd75db1c32a4165be2a1afcc5fa1
davidwillianx <davidcbsi@gmail.com>
2015-07-03 03:04:44 -0300

    no comments

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   'facebook':{
     'clientID': '461694207330940',
     'clientSecret': 'faa5dd316e2d8c9fe1c6bfc30a1ab5c5',
-    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    //'callbackURL': 'http://localhost:8080/auth/facebook/callback'
     'callbackURL': 'https://sockio.herokuapp.com/auth/facebook/callback'
     // clientID: 'MY_CLIENT_ID',
     // clientSecret: 'MY_CLIENT_SECRET',
